e0326ed3c316cb04de8e68916aafc26992706890
will xu <xuke516469920@gmail.com>
2018-12-05 15:43:46 +0800

    fix: ignore

---
+++ Log/index.js
@@ -0,0 +1,30 @@
+const log4js = require('log4js');
+const path = require('path');
+
+log4js.configure({
+  appenders: {
+    stdout: { type: 'stdout' },
+    fileInfo: {
+      type: 'file',
+      filename: path.join(__dirname, 'packager-info.log'),
+      pattern: "-yyyy-MM-dd"
+    },
+    file: {
+      type: 'file',
+      filename: path.join(__dirname, 'packager-warn.log'),
+      pattern: "-yyyy-MM-dd"
+    }
+  },
+  categories: {
+    default: { appenders: [ 'file', 'stdout' ], level: 'INFO' },
+    // pack: {}
+  },
+  pm2: true
+});
+
+
+const Log = log4js.getLogger('pack');
+
+module.exports = {
+  Log
+};
